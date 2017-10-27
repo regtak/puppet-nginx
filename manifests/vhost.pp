32ce9482b464760689674c356b0125fa85d95245
regtak <maksim.shayakhmetov@gmail.com>
2017-10-27 19:18:05 +0000

    templates

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -1,6 +1,6 @@
 define nginx::vohst(
   Integer $port                         = 80,
-  String $priority                      = '50'
+  String $priority                      = '50',
   String $server_aliases                = '',
   Boolean $enable                       = true,
   String $owner                         = $::nginx::config_owner,
@@ -10,12 +10,20 @@ define nginx::vohst(
   String $log_dir                       = $::nginx::config_log_dir,
   String $vhost_dir                     = $::nginx::config_vhost_dir,
 ) {
-    file { "${vhost_dir}/${priority}-${name}.conf":
-      ensure => file,
-      content => template("${module_name}/vhost/vhost.conf.erb"),
-      mode    => $mode,
-      owner   => $owner,
-      group   => $group,
-      notify  => Service['nginx_service'],
+  $vhost_docroot = "${::nginx::docroot}/${name}"  
+  
+  file { "${vhost_dir}/${priority}-${name}.conf":
+    ensure => file,
+    content => template("${module_name}/vhost/vhost.conf.erb"),
+    mode    => $mode,
+    owner   => $owner,
+    group   => $group,
+    notify  => Service['nginx_service'],
    }
+  file { $vhost_docroot:
+    ensure => directory,
+    mode    => '0755',
+    owner   => $owner,
+    group   => $group,
+  } 
 }
